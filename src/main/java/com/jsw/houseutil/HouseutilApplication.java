8f013fa290ef3ab4dd1344c48ce16b9305471ad1
jsw <perfectwan8765@gmail.com>
2022-01-26 20:30:17 +0900

    Init Project

---
+++ src/main/java/com/jsw/houseutil/HouseutilApplication.java
@@ -0,0 +1,13 @@
+package com.jsw.houseutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseutilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseutilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsw/houseutil/houseutil/HouseutilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsw.houseutil.houseutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseutilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
